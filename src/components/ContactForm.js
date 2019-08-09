885097fd600c57a6bb5ffce8d947b90579752f9c
Abraham <cedenoabraham@gmail.com>
2019-08-08 23:32:32 -0300

    contact form validation and submition with formik and formspree

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,82 +1,90 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
 
-function ContactForm(){
+function ContactForm(props){
+  const {
+    values: { name, email, subject, message },
+    errors,
+    touched,
+    handleChange,
+    handleSubmit,
+    isValid,
+    setFieldTouched
+  } = props;
+
+  const change = (name, e) => {
+    e.persist();
+    handleChange(e);
+    setFieldTouched(name, true, false);
+  };
   return (
-    <div>
-      <h3>
-        If you have some fancy project to develop or any question just let me
-        know by filling out the form below
-      </h3>
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        validate={values => {
-          let errors = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
+    <React.Fragment>
+      <Grid
+        item xs={12} style={{textAlign: "center"}}
       >
-        {({ isSubmitting }) => (
-          <Form>
-            <TextField
-              id="name"
-              name="name"
-              label="Name"
-              fullWidth
-
-            />
-            <ErrorMessage name="name" component="div" />
-            <TextField
-              id="email"
-              name="email"
-              label="Email"
-              fullWidth
-            />
-            <ErrorMessage name="email" component="div" />
-            <TextField
-              id="subject"
-              name="subject"
-              label="Subject"
-              fullWidth
-            />
-            <ErrorMessage name="subject" component="div" />
-            <TextField
-              id="message"
-              name="message"
-              label="Message"
-              margin="dense"
-              variant="outlined"
-              multiline
-              fullWidth
-              rowsMax="4"
-            />
-            <ErrorMessage name="message" component="div" />
-            <Button
-              type="submit"
-              variant="outlined"
-              size="large"
-              color="primary"
-              disabled={isSubmitting}>
-              Send
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </div>
+      <form
+        /*onSubmit={handleSubmit}*/
+        action="https://formspree.io/cedenoabraham@gmail.com"
+        method="POST">
+        <TextField
+          id="name"
+          name="name"
+          label="Name"
+          helperText={touched.name ? errors.name : ""}
+          error={touched.name && Boolean(errors.name)}
+          value={name}
+          onChange={change.bind(null, "name")}
+          fullWidth
+        />
+        <TextField
+          id="email"
+          name="email"
+          label="Email"
+          helperText={touched.email ? errors.email : ""}
+          error={touched.email && Boolean(errors.email)}
+          value={email}
+          onChange={change.bind(null, "email")}
+          fullWidth
+        />
+        <TextField
+          id="subject"
+          name="subject"
+          label="Subject"
+          helperText={touched.subject ? errors.subject : ""}
+          error={touched.subject && Boolean(errors.subject)}
+          value={subject}
+          onChange={change.bind(null, "subject")}
+          fullWidth
+        />
+        <TextField
+          id="message"
+          name="message"
+          label="Message"
+          margin="dense"
+          variant="outlined"
+          multiline
+          fullWidth
+          rowsMax="4"
+          rows="4"
+          helperText={touched.message ? errors.message : ""}
+          error={touched.message && Boolean(errors.message)}
+          value={message}
+          onChange={change.bind(null, "message")}
+        />
+        <Button
+          type="submit"
+          variant="outlined"
+          size="large"
+          color="primary"
+          disabled={!isValid}
+          >
+          Send
+        </Button>
+      </form>
+    </Grid>
+  </React.Fragment>
   )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import ContactForm from '../components/ContactForm';
+
+const ContactSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
+  subject: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  message: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+});
 
 const Contact = () => {
   return (
     <Layout>
-      <ContactForm />
+      <h3>If you have any cool project in mind or any question you can let me know by filling out the form below</h3>
+      <Formik
+        render={props => <ContactForm {...props} />}
+        initialValues={{ name: '', email: '', subject: '', message: '' }}
+        validationSchema={ContactSchema}
+        onSubmit={ (values, actions) => {
+          // Handle form submit, reach an endpoint, w/e.
+        }}
+      />
     </Layout>
   )
 }
